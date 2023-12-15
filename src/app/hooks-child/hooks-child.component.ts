2d5c7e881d75e76f9051be3ca8c43474a50e26b2
Mohamed Kather Sherif <mohamedkathersherif@hakunamatata.in>
2023-12-15 12:47:34 +0530

    ngOnChanges & Basics

--- src/app/hooks-child/hooks-child.component.ts
+++ src/app/hooks-child/hooks-child.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-hooks-child',
   templateUrl: './hooks-child.component.html',
   styleUrls: ['./hooks-child.component.css']
 })
-export class HooksChildComponent implements OnInit {
+export class HooksChildComponent implements OnInit, OnChanges {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  @Input('inputTitle') Title:string="";
-
+  @Input('Inputtext') Title:string="";
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges called current value", changes['Title'].currentValue);
+    console.log("ngOnChanges called first change value", changes['Title'].firstChange);
+    console.log("ngOnChanges called first change", changes['Title'].isFirstChange);
+    console.log("ngOnChanges called previous value", changes['Title'].previousValue);
+    if(!changes['Title'].firstChange){
+      console.log("not a first change")
+    }
+    if(changes['Title'].currentValue === "hello"){
+      console.log("current value is hello")
+    }
+    
+  }
 }
