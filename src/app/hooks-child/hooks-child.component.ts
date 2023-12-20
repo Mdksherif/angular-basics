40ef321c2a3e91d3b092d63993def79d8592bcd7
Mohamed Kather Sherif <mohamedkathersherif@hakunamatata.in>
2023-12-20 11:01:34 +0530

    4 imparatnt hooks updated

--- src/app/hooks-child/hooks-child.component.ts
+++ src/app/hooks-child/hooks-child.component.ts
@@ -1,30 +1,65 @@
-import { Component, OnInit,Input,OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit,Input,OnChanges, SimpleChanges, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-hooks-child',
   templateUrl: './hooks-child.component.html',
   styleUrls: ['./hooks-child.component.css']
 })
-export class HooksChildComponent implements OnInit, OnChanges {
+export class HooksChildComponent implements OnInit, OnChanges,DoCheck, OnDestroy {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  @Input('InputKeyId') keyid:string="";
 
   @Input('Inputtext') Title:string="";
+
+  @Input('InputContent') contentList:string[]=[];
+
+  @Input('Inputvalue') valueList:string[] = [];
+
+  // ngDestroy
+  
+
+  
+ 
+  contentListCount = 0
   
   ngOnChanges(changes: SimpleChanges): void {
-    console.log("ngOnChanges called current value", changes['Title'].currentValue);
-    console.log("ngOnChanges called first change value", changes['Title'].firstChange);
-    console.log("ngOnChanges called first change", changes['Title'].isFirstChange);
-    console.log("ngOnChanges called previous value", changes['Title'].previousValue);
-    if(!changes['Title'].firstChange){
-      console.log("not a first change")
-    }
-    if(changes['Title'].currentValue === "hello"){
-      console.log("current value is hello")
-    }
+    // console.log("ngOnChanges called current value", changes['Title'].currentValue);
+    console.log("on changes called ", changes['keyid']);
+    console.log("on changes called ", changes['Title'])
+    // console.log("ngOnChanges called first change value", changes['Title'].firstChange);
+    // console.log("ngOnChanges called first change", changes['Title'].isFirstChange);
+    // console.log("ngOnChanges called previous value", changes['Title'].previousValue);
+    // this console is not working because contentList is not a string is an array...
+    // console.log("ngOnChanges called for content as currentvalue", changes['contentList'].currentValue)
+    // if(!changes['Title'].firstChange){
+    //   console.log("not a first change")
+    // }
+    // if(changes['Title'].currentValue === "hello"){
+    //   console.log("current value is hello")
+    // }
     
+  }
+
+  ngOnInit(): void {
+    console.log("ngOnInit called")
+    this.Title = "hello im Title from ngOninit";
+    // if the variable is an array push the value like below 
+    this.contentList.push('ngOnInit Array pushed in contentList');
+    this.contentListCount= 1;
+  }
+  ngDoCheck(): void {
+ 
+    if(this.contentList.length > this.contentListCount){
+      console.log("ng do check called")
+      console.log("new content added")
+      this.contentListCount++;
+      console.log("incremented count list ", this.contentListCount)
+    }
+  }
+
+  ngOnDestroy(): void {
+    console.log("ng on destroy called")
   }
 }
--- src/app/hooks/hooks.component.ts
+++ src/app/hooks/hooks.component.ts
@@ -12,5 +12,43 @@ export class HooksComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  Title:string = ""
+  // ngonchnages & ng on init
+  Title:string = "hello";
+  keyid:string = "123456789";
+
+  // ngdocheck 
+  content="";
+  contentList: string[]=[];
+
+  getContent(){
+    this.contentList.push(this.content);
+    console.log("content pushed successfully")
+    this.content="";
+    console.log("content array = >", this.contentList)
+    // im creating the array for store the content list 
+  }
+  
+
+  //ngif
+  value ="";
+  valueList:string[]=[];
+  getValue(){
+    this.valueList.push(this.value);
+    this.value=""
+    console.log("value pushed => ", this.valueList)
+    console.log("value length", this.valueList.length)
+  }
+
+    // ngDestroy
+   
+    disableHooksChild = false;
+    
+
+
+
+
+
+
+
+
 }
