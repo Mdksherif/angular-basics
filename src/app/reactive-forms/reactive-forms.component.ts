fc06df8779a292691c6906225b212748d01d17dd
Mohamed Kather Sherif <mohamedkathersherif@hakunamatata.in>
2023-12-27 19:47:27 +0530

    Reactive form with validations and fetching data

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -8,33 +8,100 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ReactiveFormsComponent implements OnInit {
 
+  
+
   contactForm = new FormGroup({
-    firstName: new FormControl('Mohamed'),
-    lastName: new FormControl('sherif'),
-    age: new FormControl(),
-    gender: new FormControl(),
-    qualification: new FormControl({
-      value:'B.tech',
-      disabled:true
-    }),
+    firstName: new FormControl('',[Validators.required, Validators.maxLength(10)]),
+    lastName: new FormControl('', [Validators.required, Validators.maxLength(10), Validators.pattern(/^[a-zA-Z]+$/)]),
+    age: new FormControl('',[
+      Validators.pattern(/^\d{1,2}$/), // Allows only digits and restricts to 2 digits
+      Validators.required,
+    ]),
+    gender: new FormControl('',[Validators.required]),
+    // qualification: new FormControl({'B.tech',disabled:true}),
+    qualification: new FormControl({ value: 'B.tech', disabled: true }),
+
     isMarried: new FormControl(),
     profession: new FormControl(),
     experience: new FormControl(),
-    country: new FormControl(),
+    country: new FormControl('',[Validators.required]),
     address: new FormGroup({
-      city: new FormControl(),
+      city: new FormControl('',[Validators.required]),
       state: new FormControl(),
-      pincode: new FormControl(), 
+      pincode: new FormControl('',[Validators.required]), 
     }),
   })
+
+
+
+  handleKeyPressNum(event: KeyboardEvent): void {
+    const inputChar = String.fromCharCode(event.charCode);
+
+    // Allow only numeric characters
+    if (!/^\d+$/.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+
+  handleKeyPressAlp(event: KeyboardEvent): void {
+    const inputChar = String.fromCharCode(event.charCode);
+  
+    // Allow only alphabet characters
+    if (!/^[a-zA-Z]+$/.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+
+  handleKeyPressAlp10(event: KeyboardEvent): void {
+    const inputChar = String.fromCharCode(event.charCode);
+  
+    // Allow only alphabet characters and limit the input to 10 characters
+    if (!/^[a-zA-Z]+$/.test(inputChar) || (event.target as HTMLInputElement).value.length >= 10) {
+      event.preventDefault();
+    }
+  }
+
+  pincodeValid(event: KeyboardEvent): void {
+    const inputChar = String.fromCharCode(event.charCode);
+
+    // Allow only numeric characters
+    if (!/^\d+$/.test(inputChar) || (event.target as HTMLInputElement).value.length== 6) {
+      event.preventDefault();
+    }
+  }
+
+  
+  getCountryName(countryCode: any) {
+    switch (countryCode) {
+      case '1': return 'India';
+      case '2': return 'USA';
+      case '3': return 'UK';
+      case '4': return 'UAE';
+      case '5': return 'Australia';
+      case '6': return 'China';
+      case '7': return 'Denmark';
+      default: return '';
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  contactFormData:any
   onSubmit(){
-    console.log("Reactive Forms => ", this.contactForm.value)
+     this.contactFormData = this.contactForm.value;
+    console.log("reactive forms =>", this.contactFormData)
+    // console.log("Reactive Forms => ", this.contactForm.value)
+    this.contactForm.reset();
   }
 
 
 
+
+  
+
+
 }
+
+
