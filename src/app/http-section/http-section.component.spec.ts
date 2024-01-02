8f7d230ceb2b2a5619c24974895a929da532f20f
Mohamed Kather Sherif <mohamedkathersherif@hakunamatata.in>
2024-01-02 18:51:44 +0530

    http get completed

---
+++ src/app/http-section/http-section.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HttpSectionComponent } from './http-section.component';
+
+describe('HttpSectionComponent', () => {
+  let component: HttpSectionComponent;
+  let fixture: ComponentFixture<HttpSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HttpSectionComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HttpSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
